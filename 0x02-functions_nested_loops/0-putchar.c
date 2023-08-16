35c60d90c7bf408d36d87f075efdf4d77e133c8e
Zicoissavage <gboyegaisaac006@gmail.com>
2023-08-16 04:02:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
-int main(void);
-	{
-	printf("_putchar\n");
+/**
+ * main-check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
-
