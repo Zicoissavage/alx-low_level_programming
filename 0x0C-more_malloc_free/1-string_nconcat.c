a5459d31cc69187aa1cf05f4ac80cdb40e6ee650
zicoissavage <gboyegaisaac006@gmail.com>
2023-11-03 07:48:39 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,13 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatentes string
+ * string_nconcat - concatenates string
  * @s1: string 1
  * @s2: string 2
  * @n: number of bytes to concatenates from s2
  * Return: concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
